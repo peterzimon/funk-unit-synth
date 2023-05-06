516d12ef7251b7de6bd8eeaa30294dd8c8e36a50
Peter Zimon <peter.zimon@gmail.com>
2023-05-06 19:59:16 +0200

    Let the fuckery begin

---
+++ README.md
@@ -0,0 +1,6 @@
+# shmoergh-funk-live
+
+Version: 1.0.0
+Author: <YOU>
+Copyright: 2023
+Licence: MIT
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * shmoergh-funk-live for Raspberry Pi Pico
+ *
+ * @version     1.0.0
+ * @author      <YOU>
+ * @copyright   2023
+ * @licence     MIT
+ *
+ */
+#include "main.h"
+
+int main() {
+    // Use for debugging
+    stdio_init_all();
+
+    return 0;
+}
